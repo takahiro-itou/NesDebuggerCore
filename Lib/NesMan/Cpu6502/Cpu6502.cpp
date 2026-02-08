dddd8f2d67ae534268853e01b777561d099f3a14
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2026-02-08 18:12:59 +0900

    空のクラスを新規作成

---
+++ Lib/NesMan/Cpu6502/Cpu6502.cpp
@@ -0,0 +1,102 @@
+﻿//  -*-  coding: utf-8-with-signature;  mode: c++  -*-  //
+/*************************************************************************
+**                                                                      **
+**                  ----   NES Debugger Project   ----                  **
+**                                                                      **
+**          Copyright (C), 2026-2026, Takahiro Itou                     **
+**          All Rights Reserved.                                        **
+**                                                                      **
+**          License: (See COPYING or LICENSE files)                     **
+**          GNU Affero General Public License (AGPL) version 3,         **
+**          or (at your option) any later version.                      **
+**                                                                      **
+*************************************************************************/
+
+/**
+**      An Implementation of Cpu6502 class.
+**
+**      @file       NesMan/Cpu6502.cpp
+**/
+
+#include    "NesDbg/pch/PreCompile.h"
+
+#include    "Cpu6502.h"
+
+
+NESDBG_NAMESPACE_BEGIN
+namespace  NesMan  {
+
+namespace  {
+
+}   //  End of (Unnamed) namespace.
+
+
+//========================================================================
+//
+//    Cpu6502  class.
+//
+
+//========================================================================
+//
+//    Constructor(s) and Destructor.
+//
+
+//----------------------------------------------------------------
+//    インスタンスを初期化する
+//  （デフォルトコンストラクタ）。
+
+Cpu6502::Cpu6502()
+{
+}
+
+//----------------------------------------------------------------
+//    インスタンスを破棄する
+//  （デストラクタ）。
+//
+
+Cpu6502::~Cpu6502()
+{
+}
+
+//========================================================================
+//
+//    Public Member Functions (Implement Pure Virtual).
+//
+
+//========================================================================
+//
+//    Public Member Functions (Overrides).
+//
+
+//========================================================================
+//
+//    Public Member Functions (Pure Virtual Functions).
+//
+
+//========================================================================
+//
+//    Public Member Functions (Virtual Functions).
+//
+
+//========================================================================
+//
+//    Public Member Functions.
+//
+
+//========================================================================
+//
+//    Accessors.
+//
+
+//========================================================================
+//
+//    Protected Member Functions.
+//
+
+//========================================================================
+//
+//    For Internal Use Only.
+//
+
+}   //  End of namespace  NesMan
+NESDBG_NAMESPACE_END
---
+++ Lib/NesMan/Cpu6502/Dis6502.h
@@ -0,0 +1,130 @@
+﻿//  -*-  coding: utf-8-with-signature;  mode: c++  -*-  //
+/*************************************************************************
+**                                                                      **
+**                  ----   NES Debugger Project   ----                  **
+**                                                                      **
+**          Copyright (C), 2026-2026, Takahiro Itou                     **
+**          All Rights Reserved.                                        **
+**                                                                      **
+**          License: (See COPYING or LICENSE files)                     **
+**          GNU Affero General Public License (AGPL) version 3,         **
+**          or (at your option) any later version.                      **
+**                                                                      **
+*************************************************************************/
+
+/**
+**      An Interface of Dis6502 class.
+**
+**      @file       NesMan/Cpu6502/Dis6502.h
+**/
+
+#if !defined( NESDBG_NESMAN_CPU6502_INCLUDED_DIS_6502_H )
+#    define   NESDBG_NESMAN_CPU6502_INCLUDED_DIS_6502_H
+
+#if !defined( NESDBG_NESMAN_INCLUDED_BASE_DIS_CPU_H )
+#    include    "NesDbg/NesMan/BaseDisCpu.h"
+#endif
+
+
+NESDBG_NAMESPACE_BEGIN
+namespace  NesMan  {
+
+//========================================================================
+//
+//    Dis6502  class.
+//
+
+class  Dis6502 : public BaseDisCpu
+{
+
+//========================================================================
+//
+//    Internal Type Definitions.
+//
+private:
+
+    typedef     BaseDisCpu      Super;
+
+
+//========================================================================
+//
+//    Constructor(s) and Destructor.
+//
+public:
+
+    //----------------------------------------------------------------
+    /**   インスタンスを初期化する
+    **  （デフォルトコンストラクタ）。
+    **
+    **/
+    Dis6502();
+
+    //----------------------------------------------------------------
+    /**   インスタンスを破棄する
+    **  （デストラクタ）。
+    **
+    **/
+    virtual  ~Dis6502();
+
+//========================================================================
+//
+//    Public Member Functions (Implement Pure Virtual).
+//
+
+//========================================================================
+//
+//    Public Member Functions (Overrides).
+//
+
+//========================================================================
+//
+//    Public Member Functions (Pure Virtual Functions).
+//
+
+//========================================================================
+//
+//    Public Member Functions (Virtual Functions).
+//
+
+//========================================================================
+//
+//    Public Member Functions.
+//
+
+//========================================================================
+//
+//    Accessors.
+//
+
+//========================================================================
+//
+//    Protected Member Functions.
+//
+
+//========================================================================
+//
+//    For Internal Use Only.
+//
+
+//========================================================================
+//
+//    Member Variables.
+//
+
+//========================================================================
+//
+//    Other Features.
+//
+private:
+    typedef     Dis6502     This;
+    Dis6502             (const  This  &);
+    This &  operator =  (const  This  &);
+public:
+    //  テストクラス。  //
+    friend  class   Dis6502Test;
+};
+
+}   //  End of namespace  NesMan
+NESDBG_NAMESPACE_END
+
+#endif
