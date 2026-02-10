d203eaeb2a937aeeee64d1204fbfaeb81d233f83
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2026-02-10 23:41:43 +0900

    サンプルアプリケーションを仮実装

--- Bin/SampleApplication.cpp
+++ Bin/SampleApplication.cpp
@@ -54,5 +54,55 @@ int  main(int argc, char * argv[])
     manNes.printRegisters(std::cout)
             <<  std::endl;
 
+    NesMan::InstExecResult  ret = NesMan::InstExecResult::SUCCESS_CONTINUE;
+    NesMan::CounterInfo     ci;
+    int     cnt     = 0;
+    clock_t clkSta  = clock();
+    while ( ret != NesMan::InstExecResult::UNDEFINED_OPECODE ) {
+        ret = manNes.executeCurrentInst();
+
+        //  レジスタをダンプ。  //
+        std::cout   <<  "REGS\n";
+        manNes.printRegisters(std::cout)
+                <<  std::endl;
+
+        //  次の命令を逆アセンブル。    //
+        std::cout   <<  "Mnemonic:\t"  <<  cnt  <<  "\n";
+        manNes.writeMnemonic(std::cout, manNes.getNextPC())
+                <<  std::endl;
+
+        ++ cnt;
+        if ( !(cnt & 0x07FFFFFF) ) {
+            ci  = manNes.getCpuCounters();
+            ClockCount  cc  = ci.totalCycles;
+            clock_t clkEnd  = clock();
+            const double elapsed = static_cast<double>(clkEnd - clkSta)
+                    * 1000.0 / CLOCKS_PER_SEC;
+            std::cout   <<  "Instructions: "
+                        <<  ci.numOpeCodes  <<  ", "
+                        <<  elapsed <<  "ms : "
+                        <<  (cnt / elapsed) <<  " kHz\n"
+                        <<  "Clock Counts: "
+                        <<  cc  <<  ", "
+                        <<  elapsed <<  "ms : "
+                        <<  (cc / elapsed)  <<  " kHz"
+                        <<  std::endl;
+        }
+    }
+
+    ci  = manNes.getCpuCounters();
+    ClockCount  cc  = ci.totalCycles;
+    clock_t clkEnd  = clock();
+    const double elapsed = static_cast<double>(clkEnd - clkSta)
+                                * 1000.0 / CLOCKS_PER_SEC;
+    std::cout   <<  "Instructions: "
+                <<  cnt <<  ", "
+                <<  elapsed <<  "ms : "
+                <<  (cnt / elapsed) <<  " kHz\n"
+                <<  "Clock Counts: "
+                <<  cc  <<  ", "
+                <<  elapsed <<  "ms : "
+                <<  (cc / elapsed)  <<  " kHz"
+                <<  std::endl;
     return ( 0 );
 }
