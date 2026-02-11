52203a3e19354b77fe8886f5995378162bf93fdd
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2026-02-11 15:58:43 +0900

    ニーモニックを表示する機能の一部分を実装

--- Lib/NesMan/Cpu6502/Dis6502.cpp
+++ Lib/NesMan/Cpu6502/Dis6502.cpp
@@ -37,9 +37,47 @@ namespace  {
 CONSTEXPR_VAR   const   MnemonicMap
 dis6502Mnemonics[] = {
     //  NOP 系  //
-    { 0xFF, 0x82, "DOP" },
-    { 0xFF, 0xC2, "DOP" },
-    { 0xFF, 0xE2, "DOP" },
+    { 0x000000FF, 0x00000082, "DOP" },
+    { 0x000000FF, 0x000000C2, "DOP" },
+    { 0x000000FF, 0x000000E2, "DOP" },
+
+    { 0x000000FF, 0x00000000, "BRK" },
+    { 0x000000FF, 0x00000020, "JSR" },
+    { 0x000000FF, 0x00000040, "RTI" },
+    { 0x000000FF, 0x00000060, "RTS" },
+    { 0x000000FF, 0x00000080, "DOP" },
+    { 0x000000FF, 0x000000A0, "LDY" },
+    { 0x000000FF, 0x000000C0, "CPY" },
+    { 0x000000FF, 0x000000E0, "CPX" },
+
+    { 0x000000FF, 0x00000010, "BPL" },
+    { 0x000000FF, 0x00000030, "BMI" },
+    { 0x000000FF, 0x00000050, "BVC" },
+    { 0x000000FF, 0x00000070, "BVS" },
+    { 0x000000FF, 0x00000090, "BCC" },
+    { 0x000000FF, 0x000000B0, "BCS" },
+    { 0x000000FF, 0x000000D0, "BNE" },
+    { 0x000000FF, 0x000000F0, "BEQ" },
+
+    { 0x000000FF, 0x00000008, "PHP" },
+    { 0x000000FF, 0x00000028, "PLP" },
+    { 0x000000FF, 0x00000048, "PHA" },
+    { 0x000000FF, 0x00000068, "PLA" },
+    { 0x000000FF, 0x00000088, "DEY" },
+    { 0x000000FF, 0x000000A8, "TAY" },
+    { 0x000000FF, 0x000000C8, "INY" },
+    { 0x000000FF, 0x000000E8, "INX" },
+
+    { 0x000000FF, 0x00000018, "CLC" },
+    { 0x000000FF, 0x00000038, "SEC" },
+    { 0x000000FF, 0x00000058, "CLI" },
+    { 0x000000FF, 0x00000078, "SED" },
+    { 0x000000FF, 0x00000098, "TYA" },
+    { 0x000000FF, 0x000000B8, "CLV" },
+    { 0x000000FF, 0x000000D8, "CLD" },
+    { 0x000000FF, 0x000000F8, "SED" },
+
+    { 0x00000000, 0x00000000, "[ ??? ]" },
 };
 
 }   //  End of (Unnamed) namespace.
@@ -95,8 +133,9 @@ Dis6502::writeMnemonic(
 
     const uint32_t  opeCode = this->m_pManMem->readMemory<uint32_t>(gmAddr);
     const MnemonicMap *  oc = dis6502Mnemonics;
+    for ( ; (opeCode & oc->mask) != oc->cval; ++ oc );
 
-    const  BtByte   opSize  = g_opeCodeSize[opeCode & 0xFF];
+    const  BtByte   opSize  = g_opeCodeSize[opeCode & 0x000000FF];
     gmNext  = gmAddr + opSize;
 
     len = snprintf(dst, rem, "%04x:   %02x", gmAddr, (opeCode & 0xFF));
@@ -120,8 +159,17 @@ Dis6502::writeMnemonic(
     default:
         break;
     }
-
+    len = dst - buf;
+    while ( len < 20 ) {
+        * (dst ++)  = ' ';
+        ++ len;
+    }
     outStr  <<  buf;
+
+    const  char  *  src = oc->mnemonic;
+    len = snprintf(buf, sizeof(buf), src);
+    outStr  <<  buf  <<  std::endl;
+
     return ( outStr );
 }
 
