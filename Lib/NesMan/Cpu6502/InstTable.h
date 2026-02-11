25c6c218f59765d1e67cd0069b898a0a0dc1e1ae
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2026-02-11 13:40:00 +0900

    空のヘッダファイルを新規作成

---
+++ Lib/NesMan/Cpu6502/InstTable.h
@@ -0,0 +1,34 @@
+﻿//  -*-  coding: utf-8-with-signature;  mode: c++  -*-  //
+/*************************************************************************
+**                                                                      **
+**                  ----   NES Debugger Project   ----                  **
+**                                                                      **
+**          Copyright (C), 2026-2026, Takahiro Itou                     **
+**          All Rights Reserved.                                        **
+**                                                                      **
+**          License: (See COPYING or LICENSE files)                     **
+**          GNU Affero General Public License (AGPL) version 3,         **
+**          or (at your option) any later version.                      **
+**                                                                      **
+*************************************************************************/
+
+/**
+**      An Implementation of Thumb Instruction Table.
+**
+**      @file       NesMan/Cpu6502/InsTablet.h
+**/
+
+#if !defined( NESDBG_NESMAN_CPU6502_INCLUDED_INST_TABLE_H )
+#    define   NESDBG_NESMAN_CPU6502_INCLUDED_INST_TABLE_H
+
+
+NESDBG_NAMESPACE_BEGIN
+namespace  NesMan  {
+
+
+//========================================================================
+//
+//    OpeCode Size Table.
+//
+
+#endif
