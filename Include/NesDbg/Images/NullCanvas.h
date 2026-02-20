48fda868a55859846c4d50166145ca0914e6f768
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2026-02-20 23:37:06 +0900

    空のクラスを新規作成

---
+++ Include/NesDbg/Images/NullCanvas.h
@@ -0,0 +1,140 @@
+﻿//  -*-  coding: utf-8-with-signature;  mode: c++  -*-  //
+/*************************************************************************
+**                                                                      **
+**                  ----   NES Debugger Project   ----                  **
+**                                                                      **
+**          Copyright (C), 2026-2026, Takahiro Itou                     **
+**          All Rights Reserved.                                        **
+**                                                                      **
+**          License: (See COPYING or LICENSE files)                     **
+**          GNU Affero General Public License (AGPL) version 3,         **
+**          or (at your option) any later version.                      **
+**                                                                      **
+*************************************************************************/
+
+/**
+**      An Interface of NullCanvas class.
+**
+**      @file       Images/NullCanvas.h
+**/
+
+#if !defined( NESDBG_IMAGES_INCLUDED_NULL_CANVAS_H )
+#    define   NESDBG_IMAGES_INCLUDED_NULL_CANVAS_H
+
+#if !defined( NESDBG_IMAGES_INCLUDED_FULL_COLOR_IMAGE_H )
+#    include    "FullColorImage.h"
+#endif
+
+
+NESDBG_NAMESPACE_BEGIN
+namespace  Images  {
+
+//========================================================================
+//
+//    NullCanvas  class.
+//
+
+class  NullCanvas
+{
+
+//========================================================================
+//
+//    Constructor(s) and Destructor.
+//
+public:
+
+    //----------------------------------------------------------------
+    /**   インスタンスを初期化する
+    **  （デフォルトコンストラクタ）。
+    **
+    **/
+    NullCanvas();
+
+    //----------------------------------------------------------------
+    /**   インスタンスを破棄する
+    **  （デストラクタ）。
+    **
+    **/
+    virtual  ~NullCanvas();
+
+//========================================================================
+//
+//    Public Member Functions (Implement Pure Virtual).
+//
+
+//========================================================================
+//
+//    Public Member Functions (Overrides).
+//
+
+//========================================================================
+//
+//    Public Member Functions (Pure Virtual Functions).
+//
+
+//========================================================================
+//
+//    Public Member Functions (Virtual Functions).
+//
+public:
+
+    //----------------------------------------------------------------
+    /**   イメージを作成する。
+    **
+    **  @param [in] nWidth    イメージの幅
+    **  @param [in] nHeight   イメージの高さ
+    **  @param [in] cbPixel   ピクセル当たりのバイト数。
+    **  @param [in] lStride   行当たりのバイト数。
+    **  @param [in] lpBits    イメージデータ。
+    **/
+    virtual  void
+    createImage(
+            const  int  nWidth,
+            const  int  nHeight,
+            const  int  cbPixel,
+            const  int  lStride,
+            void  *     lpBits);
+
+
+//========================================================================
+//
+//    Public Member Functions.
+//
+
+//========================================================================
+//
+//    Accessors.
+//
+
+//========================================================================
+//
+//    Protected Member Functions.
+//
+
+//========================================================================
+//
+//    For Internal Use Only.
+//
+
+//========================================================================
+//
+//    Member Variables.
+//
+
+//========================================================================
+//
+//    Other Features.
+//
+private:
+    typedef     NullCanvas      This;
+    NullCanvas          (const  This  &);
+    This &  operator =  (const  This  &);
+public:
+    //  テストクラス。  //
+    friend  class   NullCanvasTest;
+};
+
+}   //  End of namespace  Images
+NESDBG_NAMESPACE_END
+
+#endif
---
+++ Lib/Images/NullCanvas.cpp
@@ -0,0 +1,103 @@
+﻿//  -*-  coding: utf-8-with-signature;  mode: c++  -*-  //
+/*************************************************************************
+**                                                                      **
+**                  ----   NES Debugger Project   ----                  **
+**                                                                      **
+**          Copyright (C), 2026-2026, Takahiro Itou                     **
+**          All Rights Reserved.                                        **
+**                                                                      **
+**          License: (See COPYING or LICENSE files)                     **
+**          GNU Affero General Public License (AGPL) version 3,         **
+**          or (at your option) any later version.                      **
+**                                                                      **
+*************************************************************************/
+
+/**
+**      An Implementation of NullCanvas class.
+**
+**      @file       Images/NullCanvas.cpp
+**/
+
+#include    "NesDbg/pch/PreCompile.h"
+
+#include    "NesDbg/Images/NullCanvas.h"
+
+
+NESDBG_NAMESPACE_BEGIN
+namespace  Images  {
+
+namespace  {
+
+}   //  End of (Unnamed) namespace.
+
+
+//========================================================================
+//
+//    NullCanvas  class.
+//
+
+//========================================================================
+//
+//    Constructor(s) and Destructor.
+//
+
+//----------------------------------------------------------------
+//    インスタンスを初期化する
+//  （デフォルトコンストラクタ）。
+//
+
+NullCanvas::NullCanvas()
+{
+}
+
+//----------------------------------------------------------------
+//    インスタンスを破棄する
+//  （デストラクタ）。
+//
+
+NullCanvas::~NullCanvas()
+{
+}
+
+//========================================================================
+//
+//    Public Member Functions (Implement Pure Virtual).
+//
+
+//========================================================================
+//
+//    Public Member Functions (Overrides).
+//
+
+//========================================================================
+//
+//    Public Member Functions (Pure Virtual Functions).
+//
+
+//========================================================================
+//
+//    Public Member Functions (Virtual Functions).
+//
+
+//========================================================================
+//
+//    Public Member Functions.
+//
+
+//========================================================================
+//
+//    Accessors.
+//
+
+//========================================================================
+//
+//    Protected Member Functions.
+//
+
+//========================================================================
+//
+//    For Internal Use Only.
+//
+
+}   //  End of namespace  Images
+NESDBG_NAMESPACE_END
