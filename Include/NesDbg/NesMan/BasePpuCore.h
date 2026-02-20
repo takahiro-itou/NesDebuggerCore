2d14c2703ed698525ba6c5fe37f7995bdc9ffdb6
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2026-02-20 19:10:22 +0900

    空のクラスを新規作成

---
+++ Include/NesDbg/NesMan/BasePpuCore.h
@@ -0,0 +1,126 @@
+﻿//  -*-  coding: utf-8-with-signature;  mode: c++  -*-  //
+/*************************************************************************
+**                                                                      **
+**                  ----   NES Debugger Project   ----                  **
+**                                                                      **
+**          Copyright (C), 2026-2026, Takahiro Itou                     **
+**          All Rights Reserved.                                        **
+**                                                                      **
+**          License: (See COPYING or LICENSE files)                     **
+**          GNU Affero General Public License (AGPL) version 3,         **
+**          or (at your option) any later version.                      **
+**                                                                      **
+*************************************************************************/
+
+/**
+**      An Interface of BasePpuCore class.
+**
+**      @file       NesMan/BasePpuCore.h
+**/
+
+#if !defined( NESDBG_NESMAN_INCLUDED_BASE_PPU_CORE_H )
+#    define   NESDBG_NESMAN_INCLUDED_BASE_PPU_CORE_H
+
+
+NESDBG_NAMESPACE_BEGIN
+namespace  NesMan  {
+
+
+//========================================================================
+//
+//    BasePpuCore  class.
+//
+
+class  BasePpuCore
+{
+
+//========================================================================
+//
+//    Internal Type Definitions.
+//
+
+//========================================================================
+//
+//    Constructor(s) and Destructor.
+//
+public:
+
+    //----------------------------------------------------------------
+    /**   インスタンスを初期化する
+    **  （コンストラクタ）。
+    **
+    **/
+    BasePpuCore(
+            NesManager    & manNes,
+            MemoryManager & manMem);
+
+    //----------------------------------------------------------------
+    /**   インスタンスを破棄する
+    **  （デストラクタ）。
+    **
+    **/
+    virtual  ~BasePpuCore();
+
+//========================================================================
+//
+//    Public Member Functions (Implement Pure Virtual).
+//
+
+//========================================================================
+//
+//    Public Member Functions (Overrides).
+//
+
+//========================================================================
+//
+//    Public Member Functions (Pure Virtual Functions).
+//
+
+//========================================================================
+//
+//    Public Member Functions (Virtual Functions).
+//
+
+//========================================================================
+//
+//    Public Member Functions.
+//
+
+//========================================================================
+//
+//    Accessors.
+//
+
+//========================================================================
+//
+//    Protected Member Functions.
+//
+
+//========================================================================
+//
+//    For Internal Use Only.
+//
+
+//========================================================================
+//
+//    Member Variables.
+//
+protected:
+
+//========================================================================
+//
+//    Other Features.
+//
+private:
+    typedef     BasePpuCore     This;
+    BasePpuCore         (const  This  &);
+    This &  operator =  (const  This  &);
+public:
+    //  テストクラス。  //
+    friend  class   BasePpuCoreTest;
+};
+
+}   //  End of namespace  NesMan
+NESDBG_NAMESPACE_END
+
+#endif
---
+++ Lib/NesMan/PpuNes/NesPpuImpl.cpp
@@ -0,0 +1,113 @@
+﻿//  -*-  coding: utf-8-with-signature;  mode: c++  -*-  //
+/*************************************************************************
+**                                                                      **
+**                  ----   NES Debugger Project   ----                  **
+**                                                                      **
+**          Copyright (C), 2026-2026, Takahiro Itou                     **
+**          All Rights Reserved.                                        **
+**                                                                      **
+**          License: (See COPYING or LICENSE files)                     **
+**          GNU Affero General Public License (AGPL) version 3,         **
+**          or (at your option) any later version.                      **
+**                                                                      **
+*************************************************************************/
+
+/**
+**      An Implementation of NesPpuImpl class.
+**
+**      @file       NesMan/NesPpuImpl.cpp
+**/
+
+#include    "NesDbg/pch/PreCompile.h"
+
+#include    "NesPpuImpl.h"
+
+#include    "NesDbg/NesMan/MemoryManager.h"
+#include    "InstTable.h"
+#include    "AddressingMode.h"
+#include    "ArithmeticLogic.h"
+#include    "Instructions.inl"
+
+#include    <iostream>
+
+
+NESDBG_NAMESPACE_BEGIN
+namespace  NesMan  {
+
+namespace  {
+
+}   //  End of (Unnamed) namespace.
+
+
+//========================================================================
+//
+//    NesPpuImpl  class.
+//
+
+//========================================================================
+//
+//    Constructor(s) and Destructor.
+//
+
+//----------------------------------------------------------------
+//    インスタンスを初期化する
+//  （コンストラクタ）。
+//
+
+NesPpuImpl::NesPpuImpl(
+        NesManager    & manNes,
+        MemoryManager & manMem)
+{
+}
+
+//----------------------------------------------------------------
+//    インスタンスを破棄する
+//  （デストラクタ）。
+//
+
+NesPpuImpl::~NesPpuImpl()
+{
+}
+
+//========================================================================
+//
+//    Public Member Functions (Implement Pure Virtual).
+//
+
+//========================================================================
+//
+//    Public Member Functions (Overrides).
+//
+
+//========================================================================
+//
+//    Public Member Functions (Pure Virtual Functions).
+//
+
+//========================================================================
+//
+//    Public Member Functions (Virtual Functions).
+//
+
+//========================================================================
+//
+//    Public Member Functions.
+//
+
+//========================================================================
+//
+//    Accessors.
+//
+
+//========================================================================
+//
+//    Protected Member Functions.
+//
+
+//========================================================================
+//
+//    For Internal Use Only.
+//
+
+}   //  End of namespace  NesMan
+NESDBG_NAMESPACE_END
