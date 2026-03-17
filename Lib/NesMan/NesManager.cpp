0eb1963400a73a168b2d6a9ba9ccef4e76443c6f
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2026-03-17 12:48:24 +0900

    不具合修正

--- Lib/NesMan/NesManager.cpp
+++ Lib/NesMan/NesManager.cpp
@@ -192,6 +192,18 @@ NesManager::executeCurrentInst()
 #endif
     }
 
+    while ( this->m_nCycles < 0 ) {
+        const  PpuScanLine  ppuScan = this->m_ppuCur->updateScanLine(1);
+        this->m_nCycles += 1;
+
+#if defined( _DEBUG )
+        std::cerr   <<  "CYCLES: "  <<  this->m_nCycles <<  std::endl;
+#endif
+
+        //  状況に応じて VBLANK 割り込み等を処理する。  //
+        this->m_cpuCur->performVBlankInterrupt(ppuScan);
+    }
+
     return ( ret );
 }
 
