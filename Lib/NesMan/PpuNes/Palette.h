ac8f663bbfeaee6a4e2386746946ff2cc1af25e2
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2026-03-19 22:38:53 +0900

    空のヘッダファイルを新規作成

---
+++ Lib/NesMan/PpuNes/Palette.h
@@ -0,0 +1,30 @@
+﻿//  -*-  coding: utf-8-with-signature;  mode: c++  -*-  //
+/*************************************************************************
+**                                                                      **
+**                  ----   NES Debugger Project   ----                  **
+**                                                                      **
+**          Copyright (C), 2026-2026, Takahiro Itou                     **
+**          All Rights Reserved.                                        **
+**                                                                      **
+**          License: (See COPYING or LICENSE files)                     **
+**          GNU Affero General Public License (AGPL) version 3,         **
+**          or (at your option) any later version.                      **
+**                                                                      **
+*************************************************************************/
+
+/**
+**      An Interface of Palettes.
+**
+**      @file       NesMan/PpuNes/Palette.h
+**/
+
+#if !defined( NESDBG_NESMAN_INCLUDED_PALETTE_H )
+#    define   NESDBG_NESMAN_INCLUDED_PALETTE_H
+
+NESDBG_NAMESPACE_BEGIN
+namespace  NesMan  {
+
+}   //  End of namespace  NesMan
+NESDBG_NAMESPACE_END
+
+#endif
